0c2fb599629f84a4101a0dd2a094a0c655ac15fb
rajatiwari288 <42615771+rajatiwari288@users.noreply.github.com>
2020-07-18 11:32:15 -0700

    Python-Baseball

--- stats/pitching.py
+++ stats/pitching.py
@@ -7,7 +7,7 @@ strike_outs = plays[plays['event'].str.contains('K')]
 strike_outs = strike_outs.groupby(['year','game_id']).size()
 strike_outs = strike_outs.reset_index(name='strike_outs')
 strike_outs = strike_outs.loc[:,['year','strike_outs']].apply(pd.to_numeric)
-strike_outs.plot(x='year',y='strike_outs',label='Strike Outs',kind='scatter').legend()
+strike_outs.plot(x='year',y='strike_outs',kind='scatter').legend(['Strike Outs'])
 plt.show()
 
 
