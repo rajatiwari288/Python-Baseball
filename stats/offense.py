58ec4c0d920c44cc8e146efe5b664f2db2a31246
rajatiwari288 <42615771+rajatiwari288@users.noreply.github.com>
2020-07-18 12:51:18 -0700

    Python-Baseball

--- stats/offense.py
+++ stats/offense.py
@@ -11,8 +11,7 @@ r'^T(.*)': 'triple',
 r'^HR(.*)': 'hr'}
 hit_type = hits['event'].replace(replacements,regex = True)
 hits = hits.assign(hit_type = hit_type)
-hits = hits.groupby(['inning','hit_type']).size()
-hits = hits.reset_index(name='count')
+hits = hits.groupby(['inning','hit_type']).size().reset_index(name='count')
 pd.Categorical(hits['hit_type'],['single','double','triple','hr'])
 hits = hits.sort_values(['inning','hit_type'])
 hits = hits.pivot(index = 'inning',columns='hit_type',values ='count')
